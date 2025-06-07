19121bee66f09749d6a202981a34a30f9d4e16ca
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2025-06-07 19:02:11 +0530

    chnaged url

--- vote_tracker/urls.py
+++ vote_tracker/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.VoteView.as_view(), name='vote'),
-    path('baby/', views.GirlfriendView.as_view(), name='girlfriend'),
+    path('baby/mhbdhmb/gvnhde/gv/', views.GirlfriendView.as_view(), name='girlfriend'),
     path('api/votes/', views.get_vote_count, name='vote_count_api'),
 ]
